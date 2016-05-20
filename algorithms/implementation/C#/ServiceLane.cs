3bed07c5764eea9e3778deb84713e1de4c4ffcc9
costincaraivan <costincaraivan@gmail.com>
2016-05-20 22:41:27 +0200

    Create ServiceLane.cs

---
+++ algorithms/implementation/C#/ServiceLane.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int t = Convert.ToInt32(tokens_n[1]);
+        string[] width_temp = Console.ReadLine().Split(' ');
+        int[] width = Array.ConvertAll(width_temp,Int32.Parse);
+        for(int a0 = 0; a0 < t; a0++){
+            string[] tokens_i = Console.ReadLine().Split(' ');
+            int i = Convert.ToInt32(tokens_i[0]);
+            int j = Convert.ToInt32(tokens_i[1]);
+
+            var testCase = width.Skip(i).Take(j - i + 1);
+            //Console.WriteLine(String.Join(",", testCase.Select(p=>p.ToString()).ToArray()));
+
+            Console.WriteLine(testCase.Min());    
+        }
+    }
+}
